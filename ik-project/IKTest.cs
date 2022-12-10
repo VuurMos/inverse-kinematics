ff441837f343f12666b15f4c63d0cecbd44557c7
Vuur Mos <116036135+VuurMos@users.noreply.github.com>
2022-12-10 13:35:07 +1300

    Working on adding animation offset

--- ik-project/IKTest.cs
+++ ik-project/IKTest.cs
@@ -6,87 +6,183 @@ public class IKTest : Godot.Node2D
 	#region Settings
 	[Export]
 	private int flipped = 1;
-	//limb one = calf or forearm, limb two = thigh or upper arm
 	[Export]
-	private int limbOneLen = 100;
+	private bool animated = true;
+	//limb A = calf or forearm, limb B = thigh or upper arm
 	[Export]
-	private int limbTwoLen = 120;
+	private int limbALen = 120;
+	private float aSqr;
+	[Export]
+	private int limbBLen = 100;
+	private float bSqr;
 	private int maxLen;
 	[Export]
 	private int minLen = 20;
 	#endregion
 
-	#region Working Variables
-	private Vector2 targetPos = new Vector2();
-	private Vector2 anchorPos = new Vector2();
-	private Vector2 jointPos = new Vector2();
-	private Vector2 endPos = new Vector2();
-	private float anchorToEndAngle = 0;
-	private float anchorEndDist = 0;
-	private float anchorAngle = 0;
+	#region Movement Tracking
+	private float movingDirection = 1; //this should be velocity.angle() (as radians)
+	private float motionCounter = 0.0f;
+	#endregion
+
+	#region Position Variables
+	private Vector2 targetPosition = new Vector2();
+	private Vector2 originPosition = new Vector2();
+	private Vector2 jointPosition = new Vector2();
+	private Vector2 endPosition = new Vector2();
 	#endregion
 
 	#region Visual Indicators
-	private Position2D anchorPosInd;
+	private Position2D originPosInd;
 	private Position2D endPosInd;
 	private Position2D jointPosInd;
-	private Line2D anchorEndLine;
-	private Line2D anchorJointLine;
+	private Line2D originEndLine;
+	private Line2D originJointLine;
 	private Line2D jointEndLine;
 	#endregion
 
 	public override void _Ready()
 	{
-		maxLen = limbOneLen + limbTwoLen;
-		anchorPosInd = (Position2D) GetNode("AnchorPos");
+		GetNodes();
+		InitLenCalcs();
+	}
+
+	private void GetNodes()
+	{
+		originPosInd = (Position2D) GetNode("OriginPos");
 		endPosInd = (Position2D) GetNode("EndPos");
 		jointPosInd = (Position2D) GetNode("JointPos");
-		anchorEndLine = (Line2D) GetNode("AnchorEndLine");
-		anchorJointLine = (Line2D) GetNode("AnchorJointLine");
+		originEndLine = (Line2D) GetNode("OriginEndLine");
+		originJointLine = (Line2D) GetNode("OriginJointLine");
 		jointEndLine = (Line2D) GetNode("JointEndLine");
 	}
 
-	public override void _PhysicsProcess(float delta) 
+	private void InitLenCalcs()
 	{
-		CalculateIK();
-		UpdateIKVisuals();
+		maxLen = limbALen + limbBLen;
+		aSqr = limbALen * limbALen;
+		bSqr = limbBLen * limbBLen;
 	}
 
-	private void CalculateIK()
-	{
-		targetPos = GetGlobalMousePosition();
-		// find the clamped end position from the anchor using the ideal end positon (mouse position)
-		anchorToEndAngle = anchorPos.AngleToPoint(targetPos);
-		anchorEndDist = Mathf.Clamp(anchorPos.DistanceTo(targetPos), minLen, maxLen);
-		float endPosX = anchorEndDist * -Mathf.Cos(anchorToEndAngle);
-		float endPosY = anchorEndDist * -Mathf.Sin(anchorToEndAngle);
-		endPos = new Vector2(endPosX, endPosY);
+	private Vector2 GetAnimtargetPosition(Vector2 vel)
+	{	
+		/* // motion counter simulation
+		motionCounter += 5.2f * Mathf.Pow(vel.Length() * 3, 0.4f);
 
-		// law of cosines to find anchor angle
-		float aSqr = limbOneLen * limbOneLen;
-		float bSqr = limbTwoLen * limbTwoLen;
-		float cSqr = anchorEndDist * anchorEndDist;
-		float clampedLawOfCosines = Mathf.Min(1, Mathf.Max(-1, (bSqr + cSqr - aSqr)/(2 * limbTwoLen * anchorEndDist)));
-		anchorAngle = flipped * Mathf.Acos(clampedLawOfCosines);
+		if (motionCounter > 360)
+		{
+			motionCounter = 0;
+		}
+		
+		float animCounter = Mathf.Deg2Rad(motionCounter);
+
+		// this updates based on movement speed
+		float gait = (Mathf.Pow(vel.Length() * 2, 0.4f));
+
+		/* float movingDirection = vel.Normalized().Angle();
+		//GD.Print(movingDirection);
+		float directionScale = Mathf.Cos(movingDirection);
+		GD.Print(directionScale); */
+
+		// this modifies the size of the gait based on leg length
+		// however it is way too large currently when using these formula
+		//float horizontalLenFactor = (maxLen / 4);
+		//float verticalLenFactor = (maxLen / 6);
+		//float horizontalLenFactor = 2;
+		//float verticalLenFactor = 2;
+
+		// this checks the movement angle to apply a modifier to the horizontal aspect
+		// of the movement animation
+		//float movingDirection = Mathf.Rad2Deg(vel.Angle());
+		// find the offset
+		//float ax = (gait * horizontalLenFactor * Mathf.Sin(motionCounter)) * Mathf.Cos(movingDirection);
+		//float ay = gait * verticalLenFactor * (-Mathf.Cos(motionCounter) - 1); */
+
+
+		//float pace = vel.Length() / 120;
+		float pace = Mathf.Pow(vel.Length() * 0.1f, 0.4f);
+
+		//The rate at which the motionCounter increases is the animation speed
+		float animSpeed = 0.5f;
+		motionCounter += animSpeed * pace; 
+
+		if (motionCounter > 360)
+		{
+			motionCounter = 0;
+		}
+
+		float animCounter = Mathf.Deg2Rad(motionCounter);
+
+		// (-Cos(animCounter) - 1) shifts the starting position of the movement
+		// so that at it's maximum, the offset reaches the target position
+		Vector2 baseOffset = new Vector2(
+			Mathf.Sin(animCounter), 
+			(-Mathf.Cos(animCounter) - 1)
+		);
+		
+		// this determines the size of x and y offset in relation to the limb length
+		Vector2 stepSize = new Vector2(
+			maxLen / 12, 
+			maxLen / 16
+		);
+
+		float ax = stepSize.x * pace * baseOffset.x;
+		float ay = stepSize.y * pace * baseOffset.y;
+		return new Vector2(ax, ay);
+	}
+
+	private void UpdateIK(Vector2 targetPos, Vector2 velocity)
+	{
+		// note: the target position has to be in range for the animated offset to work correctly.
+		if (animated)
+		{
+			var animPos = GetAnimtargetPosition(velocity);
+			targetPosition = new Vector2(targetPos.x + animPos.x, targetPos.y + animPos.y);
+		}
+		else
+		{
+			targetPosition = targetPos;
+		}
+
+		// set key variables using target position
+		// note: global position should refer to the global position of the IK limb origin point
+		float originToEndAngle = GlobalPosition.AngleToPoint(targetPosition);
+		float originEndDist = Mathf.Clamp(GlobalPosition.DistanceTo(targetPosition), minLen, maxLen);
+		float cSqr = originEndDist * originEndDist;
+
+		// find the end position as restricted by the total limb length
+		endPosition = new Vector2(
+			originEndDist * -Mathf.Cos(originToEndAngle), 
+			originEndDist * -Mathf.Sin(originToEndAngle)
+		);
+
+		// law of cosines to find origin angle
+		float lawOfCosinesCalc = (bSqr + cSqr - aSqr)/(2 * limbBLen * originEndDist);
+		float originAngle = flipped * Mathf.Acos(Mathf.Min(1, Mathf.Max(-1, lawOfCosinesCalc)));
 
 		// joint position
-		var jointPosX = (limbTwoLen * Mathf.Cos(anchorAngle - anchorToEndAngle));
-		var jointPosY = (limbTwoLen * Mathf.Sin(anchorAngle - anchorToEndAngle));
-		jointPos = new Vector2(-jointPosX, jointPosY);
+		jointPosition = new Vector2(
+			-limbBLen * Mathf.Cos(originAngle - originToEndAngle), 
+			limbBLen * Mathf.Sin(originAngle - originToEndAngle)
+		);
+
+		UpdateIKVisuals();
 	}
 
 	private void UpdateIKVisuals()
 	{
+		// note: Position refers to the local ik origin position
 		// update position indicators
-		endPosInd.Position = endPos;
-		jointPosInd.Position = jointPos;
+		originPosInd.Position = Position;
+		endPosInd.Position = endPosition;
+		jointPosInd.Position = jointPosition;
 
 		// update lines
-		anchorEndLine.SetPointPosition(0, anchorPos);
-		anchorEndLine.SetPointPosition(1, endPos);
-		anchorJointLine.SetPointPosition(0, anchorPos);
-		anchorJointLine.SetPointPosition(1, jointPos);
-		jointEndLine.SetPointPosition(0, jointPos);
-		jointEndLine.SetPointPosition(1, endPos);
+		originEndLine.SetPointPosition(0, Position);
+		originEndLine.SetPointPosition(1, endPosition);
+		originJointLine.SetPointPosition(0, Position);
+		originJointLine.SetPointPosition(1, jointPosition);
+		jointEndLine.SetPointPosition(0, jointPosition);
+		jointEndLine.SetPointPosition(1, endPosition);
 	}
 }
